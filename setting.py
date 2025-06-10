eef99bedeee7e5206f06fe3e4f41e98c5658aeb4
jingwangyeop <wlsrhkdduq2016@gmail.com>
2025-06-10 14:49:30 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -95,7 +95,7 @@ html_code = f"""
 </head>
 <body>
 	<div id="map" style="width:500px;height:400px;"></div>
-	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=발급받은 APP KEY를 넣으시면 됩니다."></script>
+	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=d920a6e3a78bfb47c41cbdfbe01e4030"></script>
 	<script>
 		var container = document.getElementById('map');
 		var options = {
