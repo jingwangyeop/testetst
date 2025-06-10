3c941dee962146fdb19472b5a192bbb2e74aea4c
jingwangyeop <wlsrhkdduq2016@gmail.com>
2025-06-10 14:57:50 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -95,19 +95,20 @@ html_code = f"""
 </head>
 <body>
 	<div id="map" style="width:500px;height:400px;"></div>
-	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=d920a6e3a78bfb47c41cbdfbe01e4030"></script>
+	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey={KAKAO_JAVASCRIPT_KEY}"></script>
 	<script>
 		var container = document.getElementById('map');
-		var options = {
+		var options = {{
 			center: new kakao.maps.LatLng(33.450701, 126.570667),
 			level: 3
-		};
+		}};
 
 		var map = new kakao.maps.Map(container, options);
 	</script>
 </body>
 </html>
 """
+
 
 # HTML 삽입
 components.html(html_code, height=450)
