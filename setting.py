a7f097882ceb255e3c0066ba545410e49864de57
jingwangyeop <wlsrhkdduq2016@gmail.com>
2025-06-10 14:46:18 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -90,21 +90,21 @@ html_code = f"""
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Kakao Map</title>
-    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey={KAKAO_JAVASCRIPT_KEY}"></script>
+	<meta charset="utf-8"/>
+	<title>Kakao 지도 시작하기</title>
 </head>
 <body>
-<div id="map" style="width:100%;height:400px;"></div>
-<script>
-    var container = document.getElementById('map');
-    var options = {{
-        center: new kakao.maps.LatLng(37.5665, 126.9780), // 서울 시청
-        level: 3
-    }};
+	<div id="map" style="width:500px;height:400px;"></div>
+	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=발급받은 APP KEY를 넣으시면 됩니다."></script>
+	<script>
+		var container = document.getElementById('map');
+		var options = {
+			center: new kakao.maps.LatLng(33.450701, 126.570667),
+			level: 3
+		};
 
-    var map = new kakao.maps.Map(container, options);
-</script>
+		var map = new kakao.maps.Map(container, options);
+	</script>
 </body>
 </html>
 """
